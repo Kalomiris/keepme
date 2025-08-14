bb43b71ac2c0ed4bb65a84060109474c3b927d02
gk <kalomirisgiannis@gmail.com>
2025-08-14 11:43:36 +0300

    same extra

---
+++ src/test/java/com/keepme/keepme/KeepmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keepme.keepme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
